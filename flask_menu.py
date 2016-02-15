ad9de811dc24d637669b02f43aa6220560606c7d
Alex <akmail255@gmail.com>
2016-02-15 08:24:04 +0300

    working with user_card template

--- flask_menu.py
+++ flask_menu.py
@@ -27,15 +27,17 @@ def show_books():
             book.status = book.is_checked_out
 
         book.expired_flag = False
+        book.warning_color = 'white'
         if book.date_of_return:
             # Проверяем, не просрочена ли книга:
+            print("XS: " + str(book.date_of_return))
             if int(book.date_of_return) < datetime.datetime.now().timestamp():
                 book.expired_flag = True
                 book.warning_color = 'red'
             # Переводим дату возврата из Unix timestamp:
-            book.date_of_return = datetime.datetime.fromtimestamp(int(book.date_of_return)).strftime("%Y-%m-%d")
+            book.date_of_return_str = datetime.datetime.fromtimestamp(int(book.date_of_return)).strftime("%Y-%m-%d")
         else:
-            book.date_of_return = '-'
+            book.date_of_return_str = '-'
 
     return render_template('show_books.html', lib_name=library.name_of_lib, books=books_list)
 
